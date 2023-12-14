09dc95b5174612137c59ad3e38451d11dbb1bb4e
ex_zhangwenlei@exiot.cmcc <15196725410@139.com>
2023-12-14 12:53:59 +0800

    feat: router重定向

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,26 +2,27 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Layout from '@/layout/index.vue';
 import Error01 from '@/views/Error-1/index.vue';
 const routes = [
-  {
-    path: '/',
-    component: Layout,
-    children: [
-      {
-        path: '',
-        name: 'Error',
-        component: Error01,
-      },
-    ],
-  },
-  {
-    path: '/:catchAll(.*)',
-    redirect: '/'
-  }
+    {
+        path: '/',
+        component: Layout,
+        redirect: '/404page',
+        children: [
+            {
+                path: '/404page',
+                name: 'Error',
+                component: Error01,
+            },
+        ],
+    },
+    {
+        path: '/:catchAll(.*)',
+        redirect: '/'
+    }
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+    history: createWebHistory(),
+    routes,
 });
 
 export default router;
